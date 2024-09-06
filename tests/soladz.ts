512407a6bbd6d8b6ad7119e17f0eb7f7768055ca
RealToma <real.toma.contact@gmail.com>
2024-09-06 08:45:30 -0400

    updated logic

--- tests/soladz.ts
+++ tests/soladz.ts
@@ -1,12 +1,15 @@
-const anchor = require("@coral-xyz/anchor");
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Soladz } from "../target/types/soladz";
 
-describe("sol_adz", () => {
+describe("soladz", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const program = anchor.workspace.Soladz as Program<Soladz>;
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const program = anchor.workspace.SolAdz;
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
